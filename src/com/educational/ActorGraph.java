3f406e008a329e52d46605d83b3cb89a442f24a2
samsloboda <sam.sloboda@gmail.com>
2021-10-18 23:17:16 -0400

    Testing GitHub Commit on IntelliJ

--- src/com/educational/ActorGraph.java
+++ src/com/educational/ActorGraph.java
@@ -6,6 +6,7 @@ This is the main class of this project!
 public class ActorGraph {
     public static void main(String[] args) {
         System.out.println("Why is it not working yet?");
+        System.out.println("Testing GitHub!!!");
     }
 
 }
