6f1d4e794fcaaeee82f8f340bb05ea7cfcc130d2
samsloboda <sam.sloboda@gmail.com>
2021-10-18 23:10:17 -0400

    Initial Commit

---
+++ src/com/educational/TestClass.java
@@ -0,0 +1,8 @@
+package com.educational;
+//import edu.princeton.cs.algs4.BST;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("I work!");
+    }
+}
