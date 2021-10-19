d3feacdb76877588b03a2b5b09ece19d469e31a3
samsloboda <sam.sloboda@gmail.com>
2021-10-18 23:13:58 -0400

    Initial Commit

---
+++ src/com/educational/ActorGraph.java
@@ -0,0 +1,11 @@
+package com.educational;
+/*
+This is the main class of this project!
+ */
+
+public class ActorGraph {
+    public static void main(String[] args) {
+        System.out.println("Why is it not working yet?");
+    }
+
+}
