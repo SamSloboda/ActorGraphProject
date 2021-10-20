6385602cc57154b8100633f154535eae0b26503f
samsloboda <sam.sloboda@gmail.com>
2021-10-20 00:33:33 -0400

    Added data to Array and HashMap. Error found the Array size does not correspond with the HashMap size. Potential fix, either hardcode the number for this dataset, or copy only HashMap.Size() elements to new array...not a big bug now, will be fixed later.

--- src/com/educational/ActorGraph.java
+++ src/com/educational/ActorGraph.java
@@ -8,5 +8,7 @@ import java.util.ArrayList;
 public class ActorGraph {
     public static void main(String[] args) {
         BuildDataStructs bds = new BuildDataStructs("/Users/samsloboda/OneDrive - Shepherd University/4 Shepherd Senior/Fall 2021/CIS431/Projects/ActorGraphProject/src/com/educational/test_movies.txt");
+        bds.printArraySize();
+        bds.printHashMapSize();
     }
 }
--- src/com/educational/BuildDataStructs.java
+++ src/com/educational/BuildDataStructs.java
@@ -3,12 +3,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BuildDataStructs {
-    private ArrayList parsedArray;
+    private ArrayList<Vertex> parsedArray;
     private String pathName;
+    HashMap<String, Integer> stringToIdHashMap;
+    String[] idToStringArray;
+
 
     public BuildDataStructs(String path){
         this.pathName=path;
         parsedArray = getParsedArray();
+
+        buildTables();
+
 
 
     }
@@ -26,8 +32,32 @@ public class BuildDataStructs {
     private void buildTables(){
         int lengthOfArray = parsedArray.size();
         int capacityOfHashMap = parsedArray.size() * 2;
-        String[] idToStringArray = new String[lengthOfArray];
-        HashMap<String, Integer> stringToIdHashMap = new HashMap<String, Integer>(capacityOfHashMap);
+        idToStringArray = new String[lengthOfArray];
+        stringToIdHashMap = new HashMap<String, Integer>(capacityOfHashMap);
 
+        int idCount = 0;
+        for(Vertex v: parsedArray){
+            if(stringToIdHashMap.isEmpty() || !stringToIdHashMap.containsKey(v.getName())){
+                v.setID(idCount);
+                idToStringArray[v.getID()] = v.getName();
+                stringToIdHashMap.put(v.getName(),v.getID());
+                idCount++;
+            }
+        }
     }
+    public void printHashMap(){
+        System.out.println(stringToIdHashMap);
+    }
+    public void printHashMapSize(){
+        System.out.println("Size of HashMap = "+stringToIdHashMap.size());
+    }
+    public void printArray(){
+        for(int i = 0;i<idToStringArray.length;i++){
+            System.out.println("["+i+"] = "+idToStringArray[i]);
+        }
+    }
+    public void printArraySize(){
+        System.out.println("Size of Array = "+idToStringArray.length);
+    }
+
 }
